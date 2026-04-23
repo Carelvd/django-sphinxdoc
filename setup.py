f8fdd7d40283f6b4f53851035a39e07a79e75122
Carel van Dam <carelvdam@gmail.com>
2026-04-23 08:06:41 +0200

    Requirements
    Updated the package requirements to include ansi2html for the console output.

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     install_requires=[
         'Django>=2.0',
         'django-haystack>=2.8.0',
+        'ansi2html>=1.9.2',
     ],
     packages=find_packages(where='src'), # , exclude=('tests',)
     package_dir={'': 'src'},
