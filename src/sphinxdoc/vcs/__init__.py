ef9d9f066ad2eb8ed82492f73dcf55111e6bf78a
Carel van Dam <carelvdam@gmail.com>
2026-04-23 09:34:54 +0200

    Version Control Systems: Git
    The setup script, setup.py, was not including the namespaced `vcs` package modules. I've converted this to a standard package in the interim.

---
+++ src/sphinxdoc/vcs/__init__.py
@@ -0,0 +1,13 @@
+"""\
+Version Control Systems
+
+This makes provision for the user to pull down version controlled sources of documentation.
+
+This package will eventually be expanded upon to support version control systems.
+
+.. TODO :: 
+
+   Consider removing this init file to make this a namespaced package.
+   Should you do so be sure to update the setup.py file to use `find_namspaced_packages` instead of `find_packages`
+
+"""
